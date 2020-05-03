2ae581c9c2339730ef59f8433fc4166d2b339f98
alehechka <ajlehechka@gmail.com>
2020-05-02 21:59:31 -0500

    Added functions and new CI-CD

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -13,7 +13,18 @@ const firebaseConfig = {
   appId: "1:881279966574:web:8a35962da4c411e69c357f",
 };
 
-firebase.initializeApp(firebaseConfig);
+const firebaseConfig_dev = {
+  apiKey: "AIzaSyBxrtuaG2KspaLvF6itDfb_GNsxIPzeYiQ",
+  authDomain: "yt-react-redux-dev.firebaseapp.com",
+  databaseURL: "https://yt-react-redux-dev.firebaseio.com",
+  projectId: "yt-react-redux-dev",
+  storageBucket: "yt-react-redux-dev.appspot.com",
+  messagingSenderId: "79591799435",
+  appId: "1:79591799435:web:be7281c2d7858d38a2f28e",
+  measurementId: "G-39KFCZKZ9Z"
+};
+
+firebase.initializeApp(process.env.NODE_ENV === "production" ? firebaseConfig : firebaseConfig_dev);
 firebase.firestore().settings({});
 
 firebase.firestore().enablePersistence()
